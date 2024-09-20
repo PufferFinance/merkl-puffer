753f80957f8e213e9c00f5934ea8d718ac199a4f
sheykei <clement4saunier@gmail.com>
2024-09-20 14:42:27 +0200

    rm

--- dappkit/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-import { generateTailwindConfig } from "./src/utils/tailwind";
-
-export default {
-  content: ["./{app,src}/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: generateTailwindConfig(),
-  plugins: [],
-} satisfies Config;
