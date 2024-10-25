2a1f888dc4a7e48535fcf70b13cf3b1a191de46f
sheykei <clement4saunier@gmail.com>
2024-10-25 14:19:50 +0200

    add: rough prototype

--- app/root.tsx
+++ app/root.tsx
@@ -2,13 +2,10 @@ import type { LinksFunction } from "@remix-run/node";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import "./tailwind.css";
 import { DAppProvider } from "dappkit";
-// import Footer from "src/components/layout/Footer";
-// import Header from "src/components/layout/Header";
 import { http, createConfig } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { coinbaseWallet, walletConnect } from "wagmi/connectors";
 import styles from "./tailwind.css?url";
-import { createColoring } from "dappkit";
 import { zkSyncThemes } from "src/zksync/components/layout/themes";
 
 export const config = createConfig({
@@ -67,23 +64,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
-
-export const themes = {
-  ignite: {
-    base: createColoring(["#000000", "#1755F4", "#000000"], ["#6e4c18", "#FB9901", "#FCF8F5"]),
-    info: createColoring(["#2ABDFF", "#2ABDFF", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-    good: createColoring(["#40B66B", "#40B66B", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-    warn: createColoring(["#ff9600", "#ff9600", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-    harm: createColoring(["#d22e14", "#d22e14", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-  },
-  zksync: {
-    base: createColoring(["#000000", "#1755F4", "#000000"], ["#6e4c18", "#FB9901", "#FCF8F5"]),
-    info: createColoring(["#2ABDFF", "#2ABDFF", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-    good: createColoring(["#40B66B", "#40B66B", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-    warn: createColoring(["#ff9600", "#ff9600", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-    harm: createColoring(["#d22e14", "#d22e14", "#131620"], ["#FFFFFF", "#40B66B", "white"]),
-  },
-} as const;
 
 export default function App() {
   return (
