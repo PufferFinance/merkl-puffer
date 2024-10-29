38a41ad39f6137767df59e082d96aa76ea81150e
sheykei <clement4saunier@gmail.com>
2024-10-29 12:50:00 +0100

    add: campaign display with updated api

---
+++ src/zksync/layout/Header.tsx
@@ -0,0 +1,12 @@
+import { Group, Title, Button } from "dappkit";
+
+export default function Header() {
+    return <header className="flex flex-nowrap justify-center items-center w-full">
+    <Group>
+      <Title h={3}>ZKSync</Title>
+    </Group>
+    <Group>
+      <Button look="base">Apply</Button>
+    </Group>
+  </header>
+}
