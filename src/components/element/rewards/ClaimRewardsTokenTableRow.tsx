e4ff5db1a96a7c7a11fa94b9d2feeacd0b4f7573
sheykei <clement4saunier@gmail.com>
2024-11-21 12:01:47 +0100

    add: rewards table

--- src/components/element/rewards/ClaimRewardsTokenTableRow.tsx
+++ src/components/element/rewards/ClaimRewardsTokenTableRow.tsx
@@ -1,6 +1,6 @@
 import { Checkbox, Space, Text, Value } from "dappkit";
 import Collapsible from "packages/dappkit/src/components/primitives/Collapsible";
-import { type PropsWithChildren, useState } from "react";
+import { type PropsWithChildren, useMemo, useState } from "react";
 import Token from "../token/Token";
 import { ClaimRewardsTokenRow } from "./ClaimRewardsTokenTable";
 import { Reward } from "@angleprotocol/merkl-api";
@@ -13,19 +13,23 @@ export type ClaimRewardsTokenTableRowProps = PropsWithChildren & {
 export default function ClaimRewardsTokenTableRow({reward, ...props}: ClaimRewardsTokenTableRowProps) {
   const [open, setOpen] = useState(false);
 
+  const unclaimed = useMemo(() => 
+    reward.amount - reward.claimed
+  , [reward])
+
   return (
     <ClaimRewardsTokenRow
       data-look={props?.look ?? "none"}
       {...props}
       onClick={() => setOpen(o => !o)}
       tokenColumn={<Token token={reward.token}/>}
-      amountColumn={<Value format="0,0.X">{formatUnits(reward.amount, reward.token.decimals)}</Value>}
-      claimedColumn={<Value format="0,0.X">{formatUnits(reward.claimed, reward.token.decimals)}</Value>}
+      amountColumn={<Value format="0,0.###">{formatUnits(unclaimed, reward.token.decimals)}</Value>}
+      claimedColumn={<Value format="0,0.###">{formatUnits(reward.claimed, reward.token.decimals)}</Value>}
       claimColumn={<Checkbox/>}
     >
       <Collapsible state={[open, setOpen]}>
         <Space size="md" />
-        {reward.breakdowns.sort().map(b => <Text>{b.opportunity?.name}</Text>)}
+        {reward.breakdowns.sort().map(b => <Text key={b.opportunity?.name}>{b.opportunity?.name}</Text>)}
       </Collapsible>
     </ClaimRewardsTokenRow>
   );
