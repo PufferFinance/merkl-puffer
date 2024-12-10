b07f9fb94d9ca3fb1c8966ab1d15ae600917e509
Nick Guest <nicholas.guest@gmail.com>
2024-12-10 11:48:04 +0100

    correct font-paths and override default fonts in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ export default {
     "./{src,packages}/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
     "!./packages/**/node_modules/**/*.{html,js,jsx,ts,tsx}",
   ],
-  theme: generateTailwindConfig(),
+  theme: { ...generateTailwindConfig(), 
+    fontFamily: { ...generateTailwindConfig().fontFamily,   
+    title: [ '"Recoleta", serif' ],
+    text: [ '"Inter", sans-serif' ],
+    mono: [ '"Space Mono", sans-serif' ] } },
   plugins: [],
 } satisfies Config;
